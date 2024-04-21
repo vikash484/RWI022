2bb2c53d7f8616fd8337b9654896d33b9e4efd94
vikash kumar gupta <166816541+vikash484@users.noreply.github.com>
2024-04-22 01:55:41 +0530

    Add files via upload
    string buffer

---
+++ stringbuffer.java
@@ -0,0 +1,8 @@
+public class stringbuffer {
+    public static void main(String[] args) {
+
+        StringBuffer Sb= new StringBuffer("hello vikash");
+        System.out.println(Sb.length());
+    }
+    
+}
