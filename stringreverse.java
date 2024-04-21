4db09c9b1c5bc2a910a5b00692952b17f27b3f5b
vikash kumar gupta <166816541+vikash484@users.noreply.github.com>
2024-04-22 01:06:53 +0530

    Add files via upload
    string reverse topic

---
+++ stringreverse.java
@@ -0,0 +1,7 @@
+public class stringreverse {
+    
+    public static void main(String[] args) {
+        StringBuffer r = new StringBuffer("Railworld india");
+        System.out.println(r.reverse());
+    }
+}
