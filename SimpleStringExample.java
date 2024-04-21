ab816b578ef09d68db323a23d3ff52b6632b8082
vikash kumar gupta <166816541+vikash484@users.noreply.github.com>
2024-04-22 01:17:21 +0530

    Add files via upload
    simple string

---
+++ SimpleStringExample.java
@@ -0,0 +1,9 @@
+public class SimpleStringProgram {
+    public static void main(String[] args) {
+        // Define a string
+        String message = "Hello, world!";
+
+        // Print the string
+        System.out.println(message);
+    }
+}
