5669c9a97823dcb71fcda2bbc3e8019993e4699d
vikash kumar gupta <166816541+vikash484@users.noreply.github.com>
2024-04-22 12:54:31 +0530

    Add files via upload
    array

---
+++ ArrayExample.java
@@ -0,0 +1,15 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 74;
+        marks[2] = 97;
+       // System.out.println(marks[0]);
+        //System.out.println(marks[1]);
+        //System.out.println(marks[2]);
+        for( int i=0; i<=3; i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
